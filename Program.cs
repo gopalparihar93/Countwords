cebbe18ff2486cc5cf9b75fa62546fa44c686d6b
gopalparihar93 <gopalparihar93@gmail.com>
2017-03-03 12:42:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gopal_countWords_of_sentence
+{
+    class Program
+    {
+        static void Main()
+        {
+            string sentece = "i am Gopal.The DEMO Brand website provides information about launch, pitch, attend and sponsorship opportunities for DEMO events around the world.";
+            string[] Words = sentece.Split(new char[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+           Console.WriteLine("Words of sentence are {0}", Words.Count());
+           string[] sentence = sentece.Split(new char[] {  '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+           foreach (var iten in sentence)
+           {
+               Console.Write(iten);
+               Console.WriteLine();
+           }
+          
+        }
+    }
+}
